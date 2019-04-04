ab0c396be5db66db6fde654a2004cf1a452a7a58
atwo6777 <haystackjma@gmail.com>
2019-04-03 20:30:01 -0700

    sure

---
+++ jena.java
@@ -0,0 +1,7 @@
+// Model model = ModelFactory.createDefaultModel() ;
+// model.read("firebolt.json") ;
+public class jena {
+    public static void main(String[] args) {
+      System.out.println("Hello World");
+    }
+  }
